722beb5f55fa87636d1c403f2162bad5d78aebb7
Aidan Cooper <aidan@aidancooper.co.uk>
2023-04-02 11:10:26 +0100

    :alembic: Create article feature image

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from schelling import Schelling
+from plots import plot_map_and_similarity_score
 
 
 def main():
-    population = 10000
-    groups = 5
+    population = 2500
+    groups = 4
     pct_empty = 0.2
-    threshold = 0.4
-    n_iterations = 20
+    threshold = [0.4, 0.42, 0.44, -0.46]
+    n_iterations = 25
     schelling = Schelling(
         population=population,
         groups=groups,
@@ -17,6 +18,11 @@ def main():
     for _ in range(n_iterations):
         schelling.run()
         mean_similarity_scores.append(schelling.mean_similarity_score())
+
+    fig_map, ax_map = plot_map_and_similarity_score(
+        schelling, mean_similarity_scores, n_iterations
+    )
+    fig_map.savefig("plots/map.png", dpi=1600)
 
 
 if __name__ == "__main__":
